750d8ac70a1b0cf8796431a6d4a6cfd975ac2624
Pathan-Faizann <faizanpathan8104@gmail.com>
2025-09-02 15:50:31 +0530

    updated

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import "./Details.css"
 
 const Payment = () => {
-    const [addr,setAddr] = useState([])
+    // const [addr,setAddr] = useState([])
   return (
     <div className='body'>
         <div className='row mt-3 form-1 justify-content-center'>
@@ -10,9 +10,9 @@ const Payment = () => {
         <div className='form rounded  p-4'>
           <h3 className='my-4 text-center'>Contact Details :</h3>
             <label className='form-label fs-4'>Ph-number :</label>
-            <input type="number" max={10} placeholder='ph-number' className='form-control bg-transparent rounded-pill' />
+            <input type="number" max={10} placeholder='ph-number' className='form-control  rounded-pill' />
             <label className='form-label fs-4'>E-mail :</label>
-            <input type="email" placeholder='your@gmail.com' className='form-control bg-transparent rounded-pill' />
+            <input type="email" placeholder='your@gmail.com' className='form-control  rounded-pill' />
         </div> 
         </div>
         </div>
@@ -22,16 +22,16 @@ const Payment = () => {
           <h3 className='my-4 mt-5 text-center'>Address :</h3>
            
               <label htmlFor="" className='form-label mt-3 fs-4'>House number : </label>
-              <input type="text" placeholder='House number and Street name' className=' form-control bg-transparent rounded-pill' />
+              <input type="text" placeholder='House number and Street name' className=' form-control  rounded-pill' />
              
                 <label htmlFor="" className='form-label mt-3 fs-4'>Area name :</label>
-                <input type="text" placeholder='Area name' className='form-control bg-transparent rounded-pill' />
+                <input type="text" placeholder='Area name' className='form-control  rounded-pill' />
              
                 <label htmlFor="" className='form-label mt-3 fs-4'>Landmark</label>
-                <input type="text" placeholder='Landmark' className=' form-control bg-transparent rounded-pill' />
+                <input type="text" placeholder='Landmark' className=' form-control  rounded-pill' />
              
                 <label htmlFor="" className='form-label mt-3 fs-4'>Pincode</label>
-                <input type="number" placeholder='pincode' className='form-control bg-transparent rounded-pill' />
+                <input type="number" placeholder='pincode' className='form-control  rounded-pill' />
                 <div className='d-flex justify-content-center'>
                 <button type='submit' className='btn btn-primary my-4 text-center'>Proceed to Payment</button>
 
